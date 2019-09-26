631855f7e876eb6357ad1a29b63863e7a0f9d9be
dgbailey <bailey.dust@gmail.com>
2019-09-26 15:11:12 -0700

    vanilla testing with jest

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,33 @@
 const helpers = require('./project-1');
 
-// start testing!
+// start testing!//toBe, toEqual, toContain, not toHaveLength
+//toEqual recursively checks every field of an object or array.
+
+//arrange ,act, assert
+describe('project1',() =>{
+    describe('multiplyByTen',() =>{
+        it('multiplies a number by 10 and returns result',()=>{
+            expect(helpers.multiplyByTen(4)).toBe(40);
+            expect(helpers.multiplyByTen(Math.max)).toBeNaN();
+        })
+    });
+});
+
+describe('project -1',() =>{
+    describe('subtractFive', ()=>{
+        it('subtracts 5 from a number',()=>{
+            expect(helpers.subtractFive(4)).toBeTruthy()
+        });
+        it('subtracts 5 from a number',()=>{
+            expect(helpers.subtractFive(Math.max)).toBeNaN()
+        });
+    });
+});
+
+describe('project-1', () =>{
+    describe('areSameLength',()=>{
+        it('checks string length equality', () =>{
+            expect(helpers.areSameLength('test','be')).toBeFalsy()
+        });
+    });
+});
