e48dcb020d7204d1e5856020b7ee354b58d8dfb7
dgbailey <bailey.dust@gmail.com>
2019-09-27 11:17:08 -0700

    more on toEqual, toBe, toBeCloseTo

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -2,6 +2,7 @@ const helpers = require('./project-1');
 
 // start testing!//toBe is ===,  Object.is() is strict equality except for how it treats NAN and +0/-0 not == no type coercion, referential equality for objects, toEqual deep comparison, toContain, not toHaveLength
 //toEqual recursively checks every field of an object or array.
+//If both operands are objects, then JavaScript compares internal references 
 /*=== is called strict comparison operator in JavaScript. Object.is and strict comparison operator behave exactly the same except for NaN and +0/-0.*/
 
 //to Equal checks the deep equality, recursively passing through the properties, === strict or abstract == is never true comparing different object instances
@@ -59,18 +60,45 @@ describe('project1',()=>{
         it('Passes if param is int GT 50. Fails: Param  LT LTE 50',()=>{
           
             const integer = Infinity
+            const expected = true
             expect(helpers.greaterThanFifty(integer)).toBeTruthy()
             
         });
         it('Passes if param is int GT 50. Fails: Param is not INT',()=>{
             const nonInt = '';
-           
+            const expected = false
             expect(helpers.greaterThanFifty(nonInt)).toBe(false)
             expect(helpers.greaterThanFifty(nonInt)).toBeFalsy()
             
         });
     });
 });
+
+
+describe('project1', ()=>{
+    describe('divide',()=>{
+        it('Divides first param by second param. Fails is non int is provided.',()=>{
+            const correctValue = 2;
+            const incorrectValue = 'test';
+            expect(helpers.divide(correctValue,incorrectValue)).toBeNaN()
+            expect(helpers.divide(correctValue,incorrectValue)).toBeNaN()
+        })
+        it('Divides first param by second param. Fails is non 0 is provided.',()=>{
+            const correctValue = 2;
+            const incorrectValue = 0;
+            expect(helpers.divide(correctValue,incorrectValue)).toBe(Infinity)
+           
+        })
+        it('Divides first param by second param. Fails  is provided.',()=>{
+            const correctValue = 2;
+            const correctValue2 = 3;
+            const expected = .662
+            expect(helpers.divide(correctValue,correctValue2)).toBeCloseTo(expected)
+           
+        })
+    })
+})
+
 
 //what is the unit under test
 //what should it do 
