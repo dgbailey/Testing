7f74f123666769f2663f4428848b10c1adffb8aa
dgbailey <bailey.dust@gmail.com>
2019-09-27 10:32:19 -0700

    vanilla tests with jest

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,7 +1,13 @@
 const helpers = require('./project-1');
 
-// start testing!//toBe, toEqual, toContain, not toHaveLength
+// start testing!//toBe is ===,  Object.is() is strict equality except for how it treats NAN and +0/-0 not == no type coercion, referential equality for objects, toEqual deep comparison, toContain, not toHaveLength
 //toEqual recursively checks every field of an object or array.
+/*=== is called strict comparison operator in JavaScript. Object.is and strict comparison operator behave exactly the same except for NaN and +0/-0.*/
+
+//to Equal checks the deep equality, recursively passing through the properties, === strict or abstract == is never true comparing different object instances
+//new keyword binds this to an empty object. most likely by calling c fn in the new object context.
+
+
 
 //arrange ,act, assert
 describe('project1',() =>{
@@ -13,7 +19,7 @@ describe('project1',() =>{
     });
 });
 
-describe('project -1',() =>{
+describe('project1',() =>{
     describe('subtractFive', ()=>{
         it('subtracts 5 from a number',()=>{
             expect(helpers.subtractFive(4)).toBeTruthy()
@@ -24,10 +30,49 @@ describe('project -1',() =>{
     });
 });
 
-describe('project-1', () =>{
+describe('project1', () =>{
     describe('areSameLength',()=>{
         it('checks string length equality', () =>{
             expect(helpers.areSameLength('test','be')).toBeFalsy()
         });
     });
 });
+//assemble act assert
+describe('project1',()=>{
+    describe('areEqual',() =>{
+        it('checks strict equality of parameters. Will fail if parameters are NOT stictly equal',()=>{
+
+                const ob1 = {'a':1};
+                const ob2 = {'a':1};
+                expect(helpers.areEqual(ob1,ob2)).not.toBeTruthy();
+           
+
+            
+            
+        })
+        
+    });
+});
+
+describe('project1',()=>{
+    describe('greaterThanFifty',() =>{
+        it('Passes if param is int GT 50. Fails: Param  LT LTE 50',()=>{
+          
+            const integer = Infinity
+            expect(helpers.greaterThanFifty(integer)).toBeTruthy()
+            
+        });
+        it('Passes if param is int GT 50. Fails: Param is not INT',()=>{
+            const nonInt = '';
+           
+            expect(helpers.greaterThanFifty(nonInt)).toBe(false)
+            expect(helpers.greaterThanFifty(nonInt)).toBeFalsy()
+            
+        });
+    });
+});
+
+//what is the unit under test
+//what should it do 
+//what was the expected output
+//how do you reproduce the failure (given)(should)
